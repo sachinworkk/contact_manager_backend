a987b74d87cb81e2dab276b56fbd9f9e5beaef00
Sachin Khadka <sachinworkk@gmail.com>
2022-01-12 20:51:39 +0545

    Add swagger documentation and routes.

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
-const config = require("./config");
-
 const express = require("express");
 const app = express();
 
+const config = require("./config");
+
+const urls = require("./api/routes/urls");
+
 const PORT = process.env.PORT || config.port;
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+/**
+ * Swagger Documentation setup.
+ */
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express API for Contact Manager",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "Development server",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ["./api/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * API Routes.
+ */
+const contactRoutes = require("./api/routes/contact");
+
+app.use(urls.CONTACTS, contactRoutes);
+
+/**
+ * Start the server on the specified port.
+ */
+app.listen(config.port, () => console.log(`Server is running on PORT ${PORT}`));
